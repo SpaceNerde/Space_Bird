b24f6c4de2c98d5adb1fc61d3f42daf6673b349e
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-03-05 16:59:54 +0100

    make bird fligh

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,11 @@ fn main() {
                 ..default()
             }))
         .add_systems(Startup, (setup, spawn_bird))
-        // use inbuild bevy system to close game on esc
-        .add_systems(Update, bevy::window::close_on_esc)
+        .add_systems(Update, (
+            // use inbuild bevy system to close game on esc
+            bevy::window::close_on_esc,
+            player_movement
+        ))
         .run();
 }
 
@@ -27,12 +30,18 @@ fn setup(
     commands.spawn(Camera2dBundle::default());
 }
 
+#[derive(Component)]
+struct Bird {
+    pub jumping: bool,
+    pub velocity: f32,
+}
+
 // creates player character
 fn spawn_bird(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
-    commands.spawn(
+    commands.spawn((
         SpriteBundle {
             transform: Transform {
                 translation: Vec3::new(0.0, 0.0, 0.0),
@@ -42,8 +51,38 @@ fn spawn_bird(
             sprite: Sprite {
                 custom_size: Some(Vec2::new(125., 125.)),
                 ..default()
-            },
-            ..default()
+            }, 
+            ..default() 
+        },
+        Bird {
+            jumping: false,
+            velocity: 0.,
         }
-    );
-}   
+    ));
+} 
+
+fn player_movement(
+    input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut Bird, &mut Transform)>,
+    time: Res<Time>
+) {
+    for (mut bird, mut transform) in query.iter_mut() {
+        if input.just_pressed(KeyCode::Space) {
+            bird.jumping = true;
+        }
+        
+        if bird.jumping {
+            bird.velocity = 1000.;
+            bird.jumping = false;
+        }
+
+        transform.translation.y += bird.velocity * time.delta_seconds();
+
+        bird.velocity -= 35.;
+
+        // cap velocity so u dont go faster then the speed of sound!
+        if bird.velocity < -1000. {
+            bird.velocity = -1000.;
+        }
+    }    
+}
